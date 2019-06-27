384828ad4379e7c1611f8c72962fca2335b3bff4
samueltrejo <samuel.trejo@s.tcatnashville.edu>
2019-06-26 19:56:11 -0500

    firebase google sign in authentication

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import fbConnection from '../helpers/data/firebase-init';
 
 import Navbar from '../components/navbar';
 import Auth from '../components/auth';
-// import Home from '../components/home';
+import Home from '../components/home';
 
 import './App.scss';
+
+fbConnection();
 
 class App extends React.Component {
   state = {
@@ -12,15 +18,31 @@ class App extends React.Component {
   }
 
   componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
 
+  componentWillUnmount() {
+    this.removeListener();
   }
 
   render() {
-    // const { authed } = this.state;
+    const { authed } = this.state;
+    const loadComponent = () => {
+      if (authed) {
+        return <Home />;
+      }
+      return <Auth />;
+    }
     return (
       <div className="App">
-        <Navbar />
-        <Auth />
+        <Navbar authed={authed} />
+        {loadComponent()}
       </div>
     );
   }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
 class Navbar extends React.Component {
+  logoutClickEvent = (event) => {
+    event.preventDefault();
+    firebase.auth().signOut();
+  }
+
   render() {
+    const { authed } = this.props;
     return (
       <div className="Navbar">
         <nav className="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
@@ -12,7 +20,9 @@ class Navbar extends React.Component {
             </button>
             <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
               <div className="navbar-nav">
-                <button id="navToBio" className="btn btn-dark" onClick={this.logoutClickEvent}>Logout</button>
+                {authed ? (
+                  <button id="navToBio" className="btn btn-dark" onClick={this.logoutClickEvent}>Logout</button>
+                ) : ('')}
               </div>
             </div>
           </div>
