7d5937e612fb0eee894248ec744de848e9294a09
samueltrejo <samuel.trejo@s.tcatnashville.edu>
2019-06-26 20:08:03 -0500

    returned display info after authentication

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
+import DogPen from './dog-pen';
+import dogData from '../helpers/data/dogs-data';
+import StaffRoom from './staff-room';
+import staffData from '../helpers/data/staff-data';
+
 class Home extends React.Component {
   render() {
     return (
       <div className="Home">
-        <h2>Home</h2>
+        <DogPen dogs={dogData} />
+        <StaffRoom staff={staffData} />
       </div>
     );
   }
